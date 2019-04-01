8df9e2c2c9963412dcf17cde030aa806bfe931bb
Nandhirajan P <1217688@TCS.com>
2019-04-01 21:27:41 +0530

    Initial Code

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const http = require("http").Server(app).listen(3000);
+const upload = require("express-fileupload");
+
+app.use(upload());
+
+console.log("Server Started");
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+}
+)
+
+app.post("/", function (req, res) {
+  if (req.files) {
+    //console.log(req.files);
+    const file = req.files.filename;
+    const filename = file.name;
+
+    file.mv("./upload" + filename, function (err) {
+      if (err) {
+        console.log(err);
+        res.send("error occured");
+      }
+      else {
+        res.send("Done");
+      }
+    })
+  }
+})
